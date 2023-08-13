3e03ada8f5d3ea2f51e78111c97c2a26ccbedb1a
juanse <juansee.vedia@gmail.com>
2023-08-14 11:30:31 +1200

    agregue javascript

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-const button = document.getElementById('.btn')
+let menuVisible = false;
 
-button.addEventListener("click",saludar);
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
 
-function saludar(){
-    alert("buenas");
-   
+function seleccionar(){
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("bbss");
+        habilidades[4].classList.add("react");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
 }
